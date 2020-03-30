8b56351bca6d5300ddd8b0525e93d9a0db372576
deorelaLara <deorelacooper@gmail.com>
2020-03-30 15:35:18 -0600

    Update biblio

--- biblio.py
+++ biblio.py
@@ -7,6 +7,17 @@ class PhotoBiblio(ABC):
     def search(date):
         pass
     
+    def delete(date):
+        pass
+    
+    def insert(date):
+        pass
+    
     def getPhoto(date, bibliotec):
         apod = bibliotec.search(date)
         return apod.title
+    
+    
+    def deletePhoto(date, bibliotec):
+        apod = bibliotec.delete(date)
+        return apod.date
